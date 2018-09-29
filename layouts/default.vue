bd5df5df1e8c75ca9973ecf36227286b2b678326
Byambaa <byambaa.122@gmail.com>
2018-09-29 21:25:53 +0800

    editing navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,10 +6,7 @@
                     <v-img height="40" max-width="148" src="images/logo-small.png" contain></v-img>
                     <v-spacer></v-spacer>
                     <v-toolbar-items class="hidden-sm-and-down">
-                        <v-btn flat>Нүүр</v-btn>
-                        <v-btn flat>Дэлгүүр</v-btn>
-                        <v-btn flat>Бүтээгдэхүүн</v-btn>
-                        <v-btn flat>Холбоо барих</v-btn>
+                        <v-btn v-for="item in items" :key="item.text" flat>{{ item.text }}</v-btn>
                     </v-toolbar-items>
                 </v-toolbar>
             </v-container>
@@ -24,6 +21,20 @@
 export default {
     data() {
         return {
+            items: [
+                {
+                    text: 'Нүүр'
+                },
+                {
+                    text: 'Бүтээгдэхүүн'
+                },
+                {
+                    text: 'Дэлгүүр'
+                },
+                {
+                    text: 'Холбоо барих'
+                }
+            ]
         }
     }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
+    <!-- <v-img :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`" :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`" aspect-ratio="1" class="grey lighten-2">
+        <v-layout slot="placeholder" fill-height align-center justify-center ma-0>
+            <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+        </v-layout>
+    </v-img> -->
     <v-container grid-list-lg>
-        <h2 class="headline font-weight-bold mb-3">Дэлгүүр</h2>
+        <!-- <h2 class="headline font-weight-bold mb-3">Дэлгүүр</h2>
         <v-layout row wrap>
             <v-flex lg3>
                 <v-card>
@@ -22,7 +27,7 @@
                     <v-img src="images/products/4.jpg"></v-img>
                 </v-card>
             </v-flex>
-        </v-layout>
+        </v-layout> -->
     </v-container>
 </template>
 
